4496ae0ee30f9721309ee62f128b7ef82877b0a2
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2026-02-13 14:01:13 -0300

    feat: validation user schema

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express';
+import { z } from 'zod';
 
 class UsersController {
   create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2).max(100),
+      email: z.string().email().max(150),
+      password: z.string().min(6).max(255),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
     return response.json({message: "Ok"})
   }
 }
