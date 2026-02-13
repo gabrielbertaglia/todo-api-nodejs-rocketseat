77d032718e45835b374f5c492196e8b91aca66ef
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2026-02-13 14:17:02 -0300

    feat: creating user

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
-import {hash} from 'bcrypt'
+import { hash } from 'bcrypt'
 import { z } from 'zod';
+import { prisma } from '@/database/prisma';
+import { AppError } from '@/utils/app-error';
 
 class UsersController {
   async create(request: Request, response: Response) {
@@ -12,11 +14,27 @@ class UsersController {
 
     const { name, email, password } = bodySchema.parse(request.body)
 
+    const userAlreadyExists = await prisma.user.findFirst({
+      where: {
+        email,
+      }
+    })
+
+    if (userAlreadyExists) throw new AppError('User already exists')
+
     const hashedPassword = await hash(password, 8)
 
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      }
+    })
 
+    const { password: _, ...userWithoutPassword } = user
 
-    return response.json({message: "Ok"})
+    return response.status(201).json(userWithoutPassword)
   }
 }
 
