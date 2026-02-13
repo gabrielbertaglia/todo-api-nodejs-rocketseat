33cf10d004793fbe0d56f376d649eab9ff7b5183
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2026-02-13 20:19:37 -0300

    feat: add middleware and routes teams

---
+++ src/routes/teams-routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { TeamsController } from "@/controllers/teams-controller";
+
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+
+const teamsRoutes = Router();
+
+const teamsController = new TeamsController()
+
+teamsRoutes.use(ensureAuthenticated)
+teamsRoutes.post('/', teamsController.create)
+
+export {teamsRoutes}
