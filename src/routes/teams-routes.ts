901243e23e88c2f547149effee5d1202da92ad51
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2026-02-18 15:30:25 -0300

    feat: add verify authorization

--- src/routes/teams-routes.ts
+++ src/routes/teams-routes.ts
@@ -2,12 +2,16 @@ import { Router } from "express";
 import { TeamsController } from "@/controllers/teams-controller";
 
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorizations } from "@/middlewares/verifyUserAuthorization";
 
 const teamsRoutes = Router();
 
 const teamsController = new TeamsController()
 
-teamsRoutes.use(ensureAuthenticated)
+
+teamsRoutes.use(ensureAuthenticated, verifyUserAuthorizations(['admin']))
+
 teamsRoutes.post('/', teamsController.create)
 
-export {teamsRoutes}
+
+export { teamsRoutes }
