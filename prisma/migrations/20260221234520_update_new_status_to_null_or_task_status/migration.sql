5e4576728170bb3a17adfc70777f1498663ff4b6
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2026-02-22 12:03:11 -0300

    feat: add task history when create and update task

---
+++ prisma/migrations/20260221234520_update_new_status_to_null_or_task_status/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "tasks_history" ALTER COLUMN "new_status" DROP NOT NULL;
--- src/controllers/tasks-controller.ts
+++ src/controllers/tasks-controller.ts
@@ -23,29 +23,42 @@ class TasksController {
 
     const { title, priority, status, description, teamId, userId } = bodySchema.parse(request.body)
 
-    const targetUser = await prisma.teamMember.findFirst({
-      where: {
-        teamId,
-        userId
+    const result = await prisma.$transaction(async t => {
+      const targetUser = await prisma.teamMember.findFirst({
+        where: {
+          teamId,
+          userId
+        }
+      })
+
+      if (!targetUser) {
+        throw new AppError("Usuário não pertence a esse time.")
       }
+
+      const task = await t.task.create({
+        data: {
+          title,
+          description,
+          priority,
+          status,
+          teamId,
+          userId
+        }
+      })
+
+      await t.taskHistory.create({
+        data: {
+          taskId: task.id,
+          changedBy: request.user?.id!,
+          oldStatus: status,
+          changedAt: new Date()
+        }
+      })
+
+      return task
     })
 
-    if (!targetUser) {
-      throw new AppError("Usuário não pertence a esse time.")
-    }
-
-    const task = await prisma.task.create({
-      data: {
-        title,
-        description,
-        priority,
-        status,
-        teamId,
-        userId
-      }
-    })
-
-    return response.status(201).json(task)
+    return response.status(201).json(result)
 
   }
 
@@ -61,6 +74,14 @@ class TasksController {
 
     const { teamId } = schemaParams.parse(request.params)
     const { status, priority } = querySchema.parse(request.query)
+
+    const teamExists = await prisma.team.findUnique({
+      where: { id: teamId }
+    })
+
+    if (!teamExists) {
+      throw new AppError("Time não encontrado", 404)
+    }
 
     const tasks = await prisma.task.findMany({
       where: {
@@ -88,19 +109,53 @@ class TasksController {
     const { id } = schemaParams.parse(request.params)
     const { priority, status, title, description } = bodySchema.parse(request.body)
 
-    const updated = await prisma.task.update({
-      where: {
-        id
-      },
-      data: {
-        description,
-        priority,
-        status,
-        title
+    const result = await prisma.$transaction(async t => {
+
+      const existingTask = await t.task.findUnique({
+        where: { id }
+      })
+
+      if (!existingTask) {
+        throw new AppError("Task não encontrada.", 404)
       }
+
+      const task = await t.task.findUnique({
+        where: {
+          id: id
+        }
+      })
+
+      const updatedTask = await t.task.update({
+        where: {
+          id
+        },
+        data: {
+          description,
+          priority,
+          status,
+          title
+        }
+      })
+
+
+      console.log("updatedTask.status", updatedTask.status)
+      console.log("status", status)
+      console.log("updatedTask.status !== status", updatedTask.status !== status)
+      if (task?.status !== status) {
+        await t.taskHistory.create({
+          data: {
+            taskId: id,
+            changedBy: request.user?.id!,
+            oldStatus: existingTask.status,
+            newStatus: status,
+            changedAt: new Date()
+          }
+        })
+      }
+      return updatedTask
     })
 
-    response.json(updated)
+    response.json(result)
   }
 
   async delete(request: Request, response: Response) {
