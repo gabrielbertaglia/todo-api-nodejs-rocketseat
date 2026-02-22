2098c5fdfdbde3d4a6db0fafe1f563656dc65dfb
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2026-02-22 19:00:35 -0300

    feat:(test): sessions and teams tests

---
+++ src/tests/sessions-controller.test.ts
@@ -0,0 +1,32 @@
+import { app } from '@/app'
+import { prisma } from '@/database/prisma'
+import request from 'supertest'
+
+describe("SessionsController", () => {
+
+  let user_id: string
+
+  afterAll(async () => {
+    await prisma.user.delete({
+      where: { id: user_id }
+    })
+  })
+
+  it("should authenticate and get access token", async () => {
+    const userResponse = await request(app).post('/users').send({
+      name: "Auth Test User",
+      email: "auth_test_user@teste.com",
+      password: "teste123"
+    })
+
+    user_id = userResponse.body.id
+
+    const sessionResponse = await request(app).post("/sessions").send({
+      email: "auth_test_user@teste.com",
+      password: "teste123"
+    })
+
+    expect(sessionResponse.status).toBe(200)
+    expect(sessionResponse.body.accessToken).toEqual(expect.any(String))
+  })
+})
---
+++ src/tests/teams-controller.test.ts
@@ -0,0 +1,37 @@
+import { app } from '@/app'
+import { prisma } from '@/database/prisma'
+import request from 'supertest'
+
+describe("TeamsController", () => {
+
+  let accessToken: string
+
+  beforeAll(async () => {
+    const sessionResponse = await request(app).post("/sessions").send({
+      email: "gabriel@email.com",
+      password: "123456"
+    })
+
+    accessToken = sessionResponse.body.accessToken
+  })
+
+  afterAll(async () => {
+    await prisma.team.deleteMany({
+      where: { name: 'Test Name' }
+    })
+  })
+
+  it("should create a team", async () => {
+    const teamsResponse = await request(app).post("/teams")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        name: "Test Name",
+        description: "Test Description"
+      })
+
+    console.log("teamsResponse.body", teamsResponse.body.id)
+
+
+    expect(teamsResponse.status).toBe(201)
+  })
+})
